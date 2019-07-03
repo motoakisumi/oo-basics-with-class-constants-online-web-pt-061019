7a31df4186b496ac34ec4bcc1187c18579553607
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-07-03 08:37:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,9 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
+
   BRANDS = []
-  
+
   def initialize(brand)
     @brand = brand
   end
@@ -12,10 +12,10 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
+
   def brand=(brand)
-     @brand = brand
-     BRANDS << brand if !BRANDS.include?(brand)
+    @brand = brand
+    BRANDS << brand
   end
-  
+
 end
